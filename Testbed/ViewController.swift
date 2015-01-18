be872f71ea6b13605beb6b77ca4621fe41a2efcc
Bill Weinman <wew@bearnet.com>
2015-01-18 12:03:12 -0700

    Release version

--- Testbed/ViewController.swift
+++ Testbed/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let _version = "1.0.0"
+    
+    @IBOutlet var textView: UITextView!
+
+    private func test1() {
+        for i in 1...100 {
+            message(String(format: "This is line %02d", i))
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initTextView()
+        message("Testbed \(_version)")
+        test1()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Testbed message() support
+
+    private func initTextView() {
+        // intermittent scrollview bug workaround
+        textView.scrollEnabled = false
+        textView.scrollEnabled = true
+
+        textView.editable = false
+        textView.font = UIFont(name: "Courier", size: 14.0)
     }
 
-
+    private func message( m: String ) {
+        textView.text = textView.text + m + "\n"
+        textView.scrollRangeToVisible(NSRange(location: countElements(textView.text) - 1, length: 0))
+    }
 }
-
